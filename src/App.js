fccced9ed92b02fd8a1c629ed9eae56d4c4cbe2f
Henrique Roma <roma.fulldev@gmail.com>
2021-06-21 15:48:28 -0300

    Definindo as navigations

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import UserList from './view/UserList'
+import UserForm from './view/UserForm'
+
+const Stack = createStackNavigator()
+
+export default props => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                initialRouteName="UserList">
+                <Stack.Screen 
+                    name="UserList"
+                    component={UserList}
+                />
+                <Stack.Screen 
+                    name="UserForm"
+                    component={UserForm}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
