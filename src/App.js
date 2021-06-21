6d115964e44e6ef5d762c6ba33b7f5ad1dd8330e
Henrique Roma <roma.fulldev@gmail.com>
2021-06-21 16:18:53 -0300

    Inserindo botões pra navigation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import UserList from './view/UserList'
 import UserForm from './view/UserForm'
+import { Button, Icon } from 'react-native-elements'
 
 const Stack = createStackNavigator()
 
@@ -10,16 +11,42 @@ export default props => {
     return (
         <NavigationContainer>
             <Stack.Navigator
-                initialRouteName="UserList">
+                initialRouteName="UserList"
+                screenOptions={screenOptions}>
                 <Stack.Screen 
                     name="UserList"
                     component={UserList}
+                    options={({ navigation }) => {
+                        return {
+                            title: "Lista de Usuários",
+                            headerRight: () => (
+                                <Button 
+                                    onPress={() => navigation.navigate("UserForm")}
+                                    type="clear"
+                                    icon={<Icon name="add" size={25} color="white" />}
+                                />
+                            )
+                        }
+                    }}
                 />
                 <Stack.Screen 
                     name="UserForm"
                     component={UserForm}
+                    options={{
+                        title: "Formulário de Usuários"
+                    }}
                 />
             </Stack.Navigator>
         </NavigationContainer>
     )
+}
+
+const screenOptions = {
+    headerStyle: {
+        backgroundColor: "#f4511e"
+    },
+    headerTintColor: "#fff",
+    headerTitleStyle: {
+        fontWeight: 'bold'
+    }
 }
