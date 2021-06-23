cabdff1c80617053e7203bda5b7bab3a3c24b593
Henrique Roma <roma.fulldev@gmail.com>
2021-06-23 18:48:43 -0300

    Criando lista de usuários

---
+++ src/data/Users.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        id: 1,
+        name: 'João Silva',
+        email: 'josil@empmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png'
+    },
+    {
+        id: 2,
+        name: 'Mariana Aguiar',
+        email: 'mariag@empmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635451_960_720.png'
+    },
+    {
+        id: 3,
+        name: 'Julio Fragoso',
+        email: 'julfra@empmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/10/avatar-1294776_960_720.png'
+    },
+]
